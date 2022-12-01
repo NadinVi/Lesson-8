36ecbcddeb9a1e3a830de5ded95f7968fafb49ef
Serhii Vinokhodov <vinokhodova.nadin@gmail.com>
2022-12-01 16:43:24 +0100

    add task15

--- 15/script.js
+++ 15/script.js
@@ -5,3 +5,22 @@
 //console.log(key); // eg599gb60q926j8i
 
 const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+
+
+function generateKey(length, arr) {
+   let arrNew = [];
+   for (let i = 0; i < length; i++) {
+      arrNew.push(arr[Math.floor(Math.random() * arr.length)]);
+   }
+   return arrNew;
+   
+}
+
+document.write(`<span>${arrNew.join("")}</span>`);
+
+console.log(key);
+
+
+
+
