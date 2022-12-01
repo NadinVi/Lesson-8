5e6bd186c75bc0cabb2bd831e2cf8ea338397ceb
Serhii Vinokhodov <vinokhodova.nadin@gmail.com>
2022-12-01 20:28:05 +0100

    add 16task

--- 16/script.js
+++ 16/script.js
@@ -2,3 +2,16 @@
 // sum(3) = 3
 // sum(5) = 8
 // sum(20) = 28
+
+
+function sum() {
+        let term = 1;
+         return function sum1(number) {
+            return term += number;
+        }  
+    }
+    
+    sum();
+
+    console.log(sum(2));
+    console.log(sum(4));
