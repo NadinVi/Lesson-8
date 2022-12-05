f0506073ab5b4e32be2932325de9cfff9d44fe4e
Serhii Vinokhodov <vinokhodova.nadin@gmail.com>
2022-12-05 23:27:12 +0100

    add task19

--- 19/script.js
+++ 19/script.js
@@ -4,3 +4,23 @@
 // При копировании массива – функция применяется к каждому элементу копируемого массива.
 
 // newList = copy(list, function(value){ return value*10; })
+
+const arr = [1, 2, 3, true, 4, function f1() { }, 6];
+
+function copy(list, func) {
+    const copyArr = list.slice();
+
+    if(typeof func === "function") {
+        for(let i = 0; i < copyArr.length; i++) {
+            if(typeof copyArr[i] === "number") copyArr[i] = func(copyArr[i]);
+        }
+    }
+
+    return copyArr;
+}
+
+function numMultiplier(value) {
+    return value *= 100;
+}
+
+console.log(copy(arr, numMultiplier));
